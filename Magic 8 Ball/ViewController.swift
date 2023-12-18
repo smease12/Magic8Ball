d7516725005eb6692588f70cb2c08d1e4f560507
smease12 <scmease93@gmail.com>
2023-12-17 20:34:00 -0500

    Change image on button press

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [ UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"),
+                      UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"),
+                      UIImage(imageLiteralResourceName: "ball5")
+    ]
     
 
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[1];
     }
     
 }
